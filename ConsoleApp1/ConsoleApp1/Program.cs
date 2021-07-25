bf57aa79dad17b727ae2de8a84ac0b73e80b9e69
DariusCristianGligor <darius.gligor00@e-uvt.ro>
2021-07-25 13:22:25 +0300

    schimbare din primu

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            onsole.WriteLine("hey you, Hello World!");
         }
     }
 }
