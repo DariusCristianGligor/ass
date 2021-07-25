fcfe8128f73fe797408933e36e80cb9751e221c5
DariusCristianGligor <darius.gligor00@e-uvt.ro>
2021-07-25 15:03:28 +0300

    de la 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            onsole.WriteLine("hey you, Hello World!");
+            Console.WriteLine("hey you222, Hello World!");
         }
     }
 }
